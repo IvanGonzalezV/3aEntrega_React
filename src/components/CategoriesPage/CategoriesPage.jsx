d21ccf43b8d488b5eea98dab536134b1da7389d4
Ivan Gonzalez Vargas <ronin7_@hotmail.com>
2024-01-30 00:34:47 -0600

    add_0n9

--- src/components/CategoriesPage/CategoriesPage.jsx
+++ src/components/CategoriesPage/CategoriesPage.jsx
@@ -1,33 +1,25 @@
 import { useState, useEffect } from "react";
 import { db } from "../../firebase/firebaseConfig";
-import { collection, getDocs } from "firebase/firestore";
+import { query, collection, getDocs } from "firebase/firestore";
 
 const CategoriesPage = () => {
     const [productsData, setProductsData] = useState([]);
 
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const productsCollection = collection(db, "yourCollectionName"); // Reemplaza "yourCollectionName" con el nombre de tu colección en Firestore
-                const productsSnapshot = await getDocs(productsCollection);
-                const productsList = productsSnapshot.docs.map((doc) => doc.data());
-                setProductsData(productsList);
-            } catch (error) {
-                console.error("Error fetching data:", error);
-            }
-        };
+        const getProducts = async () => {
+            const q = query(collection(db, "Products"));
+            const querySnapshot = await getDocs(q);
+            console.log(querySnapshot)
+        }
 
-        fetchData();
+        getProducts();
     }, []);
 
     return (
-        <div>
-            {/* Renderiza o utiliza los datos obtenidos según sea necesario */}
-            {productsData.map((product) => (
-                <div key={product.id}>{/* Renderiza información del producto */}</div>
-            ))}
+        <div className="categoriesPage">
+            <h1>CategoriesPage</h1>
         </div>
-    );
+    )
 };
 
-export default CategoriesPage;
+export default CategoriesPage
