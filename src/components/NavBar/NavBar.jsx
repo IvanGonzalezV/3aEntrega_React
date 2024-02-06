20be3d82e3bd56cd02b812cc92866cad1ba30fc9
Ivan Gonzalez Vargas <ronin7_@hotmail.com>
2024-02-06 17:20:12 -0600

    add_a

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,20 +1,23 @@
+// En NavBar.jsx
 import React, { useState } from "react";
 import "./NavBar.css";
 import CartWidget from "../CartWidget/CartWidget";
 import { Link } from "react-router-dom";
 import { MaterialUISwitch } from "../../App";
+import PositionedMenu from "../PositionedMenu/PositionedMenu";
 
 const NavBar = ({ onToggleDarkMode, darkMode, cartItemCount }) => {
     return (
         <nav className="NavBar">
             <ul className="nav-ul">
                 <li className="li"><Link to="/">Home</Link></li>
-                <li className="li"><Link to="/Categories">Categories</Link></li>
+                <li> {<PositionedMenu />}</li>
                 <li className="li"><Link to="/Contact">Contact</Link></li>
                 <li className="Nav"><CartWidget itemCount={cartItemCount} /></li>
-                <li className="Switch" style={{ marginTop: '-10px' }}>
+                <li className="Switch" style={{ marginTop: '10px' }}>
                     <MaterialUISwitch checked={darkMode} onChange={onToggleDarkMode} />
                 </li>
+
             </ul>
         </nav>
     );
